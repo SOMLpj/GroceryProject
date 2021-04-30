cf77e96ec75e17a035d9b66dbdaa0c39e13000c3
Percy <percy.kc.chang@gmail.com>
2021-04-29 19:11:11 -0700

    add default

--- Checkout/checkout.php
+++ Checkout/checkout.php
@@ -1,8 +1,11 @@
 <?php
-  //Sales Tax rate in San Jose: 9.25%
   //start session
   session_start();
-  //Set default timezone
+  //Get customer id
+  $_SESSION['user_id'] = 1;
+  $customerId = $_SESSION['user_id'];
+  //Set default timezone. (Since our website is only for San Jose downtown area,
+  //we only need timezone of San Jose, which is same as Los Angeles)
   date_default_timezone_set("America/Los_Angeles");
   //Get current time
   $currentTime = date("H:i:s");
@@ -30,32 +33,77 @@
 <html>
   <head>
     <title>Checkout</title>
+    <link rel="stylesheet" href="checkout.css">
   </head>
   <body>
     <h2>Checkout</h2>
-    Payment Method:
-    <br>
-    <form action="" method="post">
-      <!--Select payment-->
-      <select name="payment_option">
-      <?php
-        $sql = "SELECT payment_id, name_on_card, card_number, exp_month, exp_year, cvc_code FROM customer_payment WHERE FK_customer_id='1'";
-        $results = mysqli_query($conn, $sql);
 
-        while ($row=mysqli_fetch_assoc($results))
-        {
-          $payment_id = $row['payment_id'];
+    <?php
+      //Get default payment method and shipping address
+      $sql = "SELECT FK_payment_id, FK_address_id FROM user WHERE user_id='$customerId'";
+      $results = mysqli_query($conn, $sql);
+      if ($row=mysqli_fetch_assoc($results)) {
+        //Get id first
+        $defaultPayment_id = $row['FK_payment_id'];
+        $defaultAddress_id = $row['FK_address_id'];
+
+        $sql = "SELECT name_on_card, card_number, exp_month, exp_year, cvc_code FROM customer_payment WHERE payment_id='$defaultPayment_id'";
+
+        $results = mysqli_query($conn, $sql);
+        if ($row=mysqli_fetch_assoc($results)){
           $name_on_card = $row['name_on_card'];
           $card_number = $row['card_number'];
           $exp_month = $row['exp_month'];
           $exp_year = $row['exp_year'];
           $cvc_code = $row['cvc_code'];
-          //Create payment option
-          $paymentOption = "Name: ". $name_on_card. ", Card Number: ". $card_number. ", Expiration Month/Year: ". $exp_month. "/". $exp_year. ", CVC Code: ". $cvc_code;
-          ?>
-          <option value="<?php echo $paymentOption;?>"><?php echo $paymentOption;?></option>
+          //Create the default payment option
+          $defaultPaymentOption = "Name: ". $name_on_card. ", Card Number: ". $card_number. ", Expiration Month/Year: ". $exp_month. "/". $exp_year. ", CVC Code: ". $cvc_code;
+        }
+        $sql = "SELECT street, city, state, zip_code FROM customer_address WHERE address_id='$defaultAddress_id'";
 
-          <?php
+        $results = mysqli_query($conn, $sql);
+        if ($row=mysqli_fetch_assoc($results)){
+          $street = $row['street'];
+          $city = $row['city'];
+          $state = $row['state'];
+          $zipCode = $row['zip_code'];
+          //create the default address option
+          $defaultAddressOption = $street. ", ". $city. ", ". $state. ", ". $zipCode;
+        }
+      }
+      else {
+        echo mysqli_error($conn);
+      }
+     ?>
+
+     <form action="" method="post">
+       Payment Method:
+       <br>
+      <!--Select payment-->
+      <select name="payment_option" class="selection">
+        <!--First display the default option-->
+        <option value="<?php echo $defaultPaymentOption;?>|<?php echo $defaultPayment_id;?>"><?php echo $defaultPaymentOption;?></option>
+      <?php
+        $sql = "SELECT payment_id, name_on_card, card_number, exp_month, exp_year, cvc_code FROM customer_payment WHERE FK_customer_id='$customerId'";
+        $results = mysqli_query($conn, $sql);
+
+        while ($row=mysqli_fetch_assoc($results))
+        {
+          $payment_id = $row['payment_id'];
+          //Create other options but not include the default
+          if ($payment_id !== $defaultPayment_id){
+            $name_on_card = $row['name_on_card'];
+            $card_number = $row['card_number'];
+            $exp_month = $row['exp_month'];
+            $exp_year = $row['exp_year'];
+            $cvc_code = $row['cvc_code'];
+            //Create payment option
+            $paymentOption = "Name: ". $name_on_card. ", Card Number: ". $card_number. ", Expiration Month/Year: ". $exp_month. "/". $exp_year. ", CVC Code: ". $cvc_code;
+            ?>
+            <option value="<?php echo $paymentOption;?>|<?php echo $payment_id;?>"><?php echo $paymentOption;?></option>
+
+            <?php
+          }
         }
            ?>
       </select>
@@ -65,23 +113,26 @@
       <br>
       <!--Select address-->
       <select name="address_option">
+        <!--First display the default option-->
+        <option value="<?php echo $defaultAddressOption;?>|<?php echo $defaultAddress_id;?>"><?php echo $defaultAddressOption;?></option>
         <?php
-          $sql1 = "SELECT address_id, street, city, state, zip_code FROM customer_address WHERE FK_customer_id='1'";
-          $results1 = mysqli_query($conn, $sql1);
+          $sql = "SELECT address_id, street, city, state, zip_code FROM customer_address WHERE FK_customer_id='$customerId'";
+          $results = mysqli_query($conn, $sql);
 
-          while ($row=mysqli_fetch_assoc($results1))
+          while ($row=mysqli_fetch_assoc($results))
           {
             $address_id = $row['address_id'];
-            $street = $row['street'];
-            $city = $row['city'];
-            $state = $row['state'];
-            $zipCode = $row['zip_code'];
-
-            $addressOption = "Street: ". $street. ", City: ". $city. ", State: ". $state. ", Zip Code: ". $zipCode;
-            ?>
-            <option value="<?php echo $addressOption;?>"><?php echo $addressOption;?></option>
-
-            <?php
+            if ($address_id !== $defaultAddress_id){
+              $street = $row['street'];
+              $city = $row['city'];
+              $state = $row['state'];
+              $zipCode = $row['zip_code'];
+              //Create other options but not include the default
+              $addressOption = $street. ", ". $city. ", ". $state. ", ". $zipCode;
+              ?>
+              <option value="<?php echo $addressOption;?>|<?php echo $address_id;?>"><?php echo $addressOption;?></option>
+              <?php
+            }
           }
              ?>
       </select>
@@ -97,20 +148,26 @@
         <option><?php echo $thirdDay;?></option>
       </select>
       <br>
-      <input type="submit" value="Select">
+      <input type="submit" value="Select" class="button">
     </form>
     <br>
 
     <?php
       //Display what customer selected
       echo "Selected Payment Method: ";
-      $selectedAddress= $_POST['address_option'];
-      echo $selectedAddress;
+      $selectedPayment= $_POST['payment_option'];
+      $explode = explode('|', $selectedPayment);
+      $selectedPayment = $explode[0];
+      $selectedPaymentId = $explode[1];
+      echo $selectedPayment;
       echo "<br><br>";
 
       echo "Selected Shipping Address: ";
-      $selectedPayment = $_POST['payment_option'];
-      echo $selectedPayment;
+      $selectedAddress = $_POST['address_option'];
+      $explode = explode('|', $selectedAddress);
+      $selectedAddress = $explode[0];
+      $selectedAddressId = $explode[1];
+      echo $selectedAddress;
       echo "<br><br>";
 
       echo "Selected Delivery Date: ";
@@ -127,9 +184,9 @@
       else {
         ?>
         <form action="/placeOrder.php" method="post">
-          <input type="hidden"  name="paymentS" value="<?php echo $selectedPayment;?>">
-          <input type="hidden"  name="addressS" value="<?php echo $selectedAddress;?>">
-          <input type="hidden"  name="dateS" value="<?php echo $selectedDate;?>">
+          <input type="hidden"  name="selectedPayment" value="<?php echo $selectedPayment;?>|<?php echo $selectedPaymentId;?>">
+          <input type="hidden"  name="selectedAddress" value="<?php echo $selectedAddress;?>|<?php echo $selectedAddressId;?>">
+          <input type="hidden"  name="selectedDate" value="<?php echo $selectedDate;?>">
           <input type="submit" value="Place Order">
         </form>
         <?php
--- Checkout/placeOrder.php
+++ Checkout/placeOrder.php
@@ -1,43 +1,115 @@
 <?php
   //Start session
   session_start();
+  //Get customer id
+  $_SESSION['user_id'] = '1';
+  $customerId = $_SESSION['user_id'];
   //Set dedault timezone
   date_default_timezone_set("America/Los_Angeles");
+  //create connection
+  $conn = mysqli_connect("sql3.freesqldatabase.com:3306", "sql3402886", "gn4yJmWUfg", "sql3402886");
+  //check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  //Sales Tax rate in San Jose, 95112: 9.25%
+  $salesTax = 0.0925;
  ?>
+
 <html>
   <head>
     <title>Place Order</title>
   </head>
   <body>
     <?php
-      //create connection
-      $conn = mysqli_connect("sql3.freesqldatabase.com:3306", "sql3402886", "gn4yJmWUfg", "sql3402886");
-      //check connection
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+      $total = 0;
+      $totalPrice = 0;
+      $itemPrice = 0; //Without Tax
+      $totalWeight = 0;
+      $freeDeliveryWeight = 20; //In pounds
+      $deliveryFee = 5;
+      //Get every item id and quantity in the cart
+      $sql = "SELECT FK_product_id, quantity FROM item_in_cart WHERE FK_customer_id='$customerId'";
+      $results = mysqli_query($conn, $sql);
+      $itemsInCart = array(); //Array for save item id and quantity
+      $notEnoughStockItems = array(); //Array for save not enough stock item
+      while ($row=mysqli_fetch_array($results)) {
+        $orderProductId = $row['FK_product_id'];
+        $quantity = $row['quantity'];
+
+        //Get the item's stock from product database
+        $sql1 = "SELECT stock, product_name, price, shipping_weight FROM product WHERE product_id='$orderProductId'";
+        $results1 = mysqli_query($conn, $sql1);
+        while ($row=mysqli_fetch_assoc($results1)){
+          $stock = $row['stock'];
+          $productName = $row['product_name'];
+          $price = $row['price'];
+          $shippingWeight = $row['shipping_weight'];
+        }
+        //If the item quantity in the cart is more than the avaliable stock,
+        //this order should not be placed
+        if ($quantity > $stock){
+          array_push($notEnoughStockItems, $productName);
+        }
+        else {
+          $itemsInCart[$orderProductId] = $quantity;
+          //Make the array key = product_id, value = quantity
+
+          $itemPrice += $price * $quantity;
+          $totalWeight += $shippingWeight * $quantity;
+        }
       }
-      if (isset($_POST["paymentS"]) && isset($_POST["addressS"]) && isset($_POST["dateS"])){
-        if ($_POST["paymentS"] && $_POST["addressS"] && $_POST["dateS"]){
-          $paymentName = $_POST["paymentS"];
-          $addressName = $_POST["addressS"];
-          $dateName = $_POST["dateS"];
+
+      $total += $itemPrice;
+      $total += $total * $salesTax;
+      //Add delivery fee if total weigtht is over max free delivery weight
+      if ($totalWeight >= $freeDeliveryWeight){
+        $total += $deliveryFee;
+      }
+      $totalPrice = round("$total", 2); //Round to 2 decimal
+
+      //If there is item that does not have enough stock, print it out
+      if (!empty($notEnoughStockItems)){
+
+        echo "Sorry, for ";
+        foreach ($notEnoughStockItems as $value) {
+           echo $value . ", ";
+        }
+        echo " we do not have enough stock for your order. Please <a href=\"checkout.php\">Go Back To Shopping Cart</a>.";
+
+        exit(""); //Stop execute rest of the code, means don't place the order
+      }
+
+      if (isset($_POST["selectedPayment"]) && isset($_POST["selectedAddress"]) && isset($_POST["selectedDate"])){
+        if ($_POST["selectedPayment"] && $_POST["selectedAddress"] && $_POST["selectedDate"]){
+          $selectedPayment = $_POST["selectedPayment"];
+          $payment_explode = explode('|', $selectedPayment);
+          $selectedPayment = $payment_explode[0];
+          $selectedPaymentId = $payment_explode[1];
+
+          $selectedAddress = $_POST["selectedAddress"];
+          $address_explode = explode('|', $selectedAddress);
+          $selectedAddress = $address_explode[0];
+          $selectedAddressId = $address_explode[1];
+
+          $selectedDate = $_POST["selectedDate"];
           $orderDate = date("Y-m-d H:i:s");
 
           //Test only
           echo "<br>";
           echo $orderDate;
           echo "<br>";
-          echo $paymentName;
+          echo $selectedPayment;
           echo "<br>";
-          echo $addressName;
+          echo $selectedAddress;
           echo "<br>";
-          echo $dateName;
+          echo $selectedDate;
           echo "<br>";
 
           //Place order
-          $sql1 = "INSERT INTO customer_order (FK_customer_id, order_date, order_total, FK_status_id, order_address, order_payment, delivery_date) VALUES ('1', '$orderDate', '100.00', '1', '$addressName', '$paymentName', '$dateName')";
-          $results1 = mysqli_query($conn, $sql1);
-          if ($results1) {
+          $sql = "INSERT INTO customer_order (FK_customer_id, order_date, order_total, FK_status_id, order_address, order_payment, delivery_date) VALUES ('$customerId', '$orderDate', '$totalPrice', '1', '$selectedAddress', '$selectedPayment', '$selectedDate')";
+          $results = mysqli_query($conn, $sql);
+          if ($results) {
             echo "Order Has Been Placed!";
             $thisOrderId = mysqli_insert_id($conn);
           }
@@ -45,26 +117,48 @@
             echo mysqli_error($conn);
           }
           echo "<br>";
-          //Update stock
-          $sql2 = "UPDATE product SET stock = stock - 10 WHERE product_id = '1'";
-          $results2 = mysqli_query($conn, $sql2);
-          if ($results2){
-            echo "(Test only) Update stock";
+
+          foreach ($itemsInCart as $key => $value) {
+            //Key is product_id, value is quantity
+            //Update order item
+            $sql = "INSERT INTO order_item (FK_order_id, FK_product_id, quantity) VALUES ('$thisOrderId', '$key', '$value')";
+            $results = mysqli_query($conn, $sql);
+            //Test only
+            if ($results){
+              echo "(Test only) Update Order Item";
+            }
+            echo "<br>";
+
+            //Update stock
+            $sql = "UPDATE product SET stock = stock - $value WHERE product_id = '$key'";
+            $results = mysqli_query($conn, $sql);
+            //Test only
+            if ($results){
+              echo "(Test only) Update stock";
+            }
+            echo "<br>";
+          }
+          //Clear the user's shopping cart
+          $sql = "DELETE FROM item_in_cart WHERE FK_customer_id='$customerId'";
+          $results = mysqli_query($conn, $sql);
+          if ($results){
+            echo "(Test only) Delete item in cart";
           }
           echo "<br>";
-          //Update order item
-          $sql3 = "INSERT INTO order_item (FK_order_id, FK_product_id, quantity) VALUES ('$thisOrderId', '1', '10')";
-          $results3 = mysqli_query($conn, $sql3);
-          if ($results2){
-            echo "(Test only) Update Order Item";
+          //Update user's default payment_id and address_id
+          $sql = "UPDATE user SET FK_payment_id = '$selectedPaymentId', FK_address_id = '$selectedAddressId' WHERE user_id='$customerId'";
+          $results = mysqli_query($conn, $sql);
+          if ($results){
+            echo "(Test only) Update default id";
           }
+          echo "<br>";
+
           mysqli_close($conn); //close connection
         }
         else {
           echo "Nothing selected";
         }
       }
-      //Add to order
 
     ?>
   </body>
